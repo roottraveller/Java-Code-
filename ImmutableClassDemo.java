f5198ffcf73abdff85b35ff6ae2ce386caf8dfa6
RishiKesh Maurya <roottraveller@users.noreply.github.com>
2017-08-27 13:31:15 +0530

    Update ImmutableClassDemo.java

--- ImmutableClassDemo.java
+++ ImmutableClassDemo.java
@@ -1,11 +1,15 @@
 import java.util.*;
 import javax.annotation.*;
+
 /*
  * Make class final -> No subclass possible
  * Make all instance veriable final and private
  */
+
 @Immutable
 //https://stackoverflow.com/questions/37087809/how-to-find-out-if-a-class-is-immutable
+
+
 final class ImmutableClass {
 	private final int dummyInt;
 	private final String dummyName;
