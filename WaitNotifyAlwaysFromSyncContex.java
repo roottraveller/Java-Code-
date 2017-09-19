5d8055c33b9ac7b9f3cd5c7afe113886f8d4a823
RishiKesh Maurya <roottraveller@users.noreply.github.com>
2017-09-19 23:16:14 +0530

    Create WaitNotifyAlwaysFromSyncContex.java
    wait() and notify() must be called from syncronized contex. 
    read : https://stackoverflow.com/questions/2779484/why-must-wait-always-be-in-synchronized-block

---
+++ WaitNotifyAlwaysFromSyncContex.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class WaitNotifyAlwaysFromSyncContex {
+	public static void main(String[] args) {
+		Thread threadA = new Thread (new ThreadA());
+		Thread threadB = new Thread (new ThreadB());
+		
+		threadA.start();
+		threadB.start();
+		
+		synchronized(threadB) { //NOTE HRER
+			try {
+				System.out.println(Thread.currentThread().getName() + " Wait() Called. Waiting for threadB to complete.");
+				threadB.wait(); //AND HERE TOO
+			} catch (InterruptedException e) {
+				System.out.println("Exception thrown " + Thread.currentThread().getName() + "  ");
+				e.printStackTrace();
+			} finally {
+				System.out.println("Wait() ended.");
+			}
+		}
+	}
+}
+class ThreadA implements Runnable {
+	@Override
+	public void run() {
+		//stuff 
+		synchronized(this) { 
+			for(int i=0; i<999; ++i) {
+				//do nothing just weast time
+				System.out.println(Thread.currentThread().getName() + " Running...");
+			}
+			notifyAll();
+		}
+	}
+}
+
+class ThreadB implements Runnable {
+	@Override
+	public void run() {
+		//stuff 
+		synchronized(this) { 
+			for(int i=0; i<999; ++i) {
+				//do nothing just weast time
+				System.out.println(Thread.currentThread().getName() + " Running...");
+			}
+			notifyAll();
+		}
+	}
+}
