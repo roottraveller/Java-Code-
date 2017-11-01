1df6e25c9ad1bffd1dba5ed02bd008850b8b0499
roottraveller <roottraveller@users.noreply.github.com>
2017-11-01 21:05:35 +0530

    Update 2DArraysSort.java

--- 2DArraysSort.java
+++ 2DArraysSort.java
@@ -1,24 +1,30 @@
 import java.util.*;
 
-public class 2DArraysSort {
+/* 
+ * Author @ roottraveller
+ * July 2017
+ */
+
+public class TwoDArraysSort {
     public static void main(String[] args) {
         Integer[][] myarr = {{0, 10}, {10, 9}, {2, 9}, {3, 9}, {4, 15}, {5, 10}, {6, 4}};
 
-        for (Integer[] i : myarr) {
-            System.out.print(i[0] + "," + i[1]);
+        for (Integer[] elem : myarr) {
+            System.out.print(elem[0] + "," + elem[1]);
+            System.out.println();
         }
 
         Arrays.sort(myarr, new Comparator<Integer[]>() {
             @Override
-            public int compare(Integer[] int1, Integer[] int2) {
-                Integer numOfKeys1 = int1[1]; //column 1
-                Integer numOfKeys2 = int2[1];
-                return numOfKeys1.compareTo(numOfKeys2);
+            public int compare(Integer[] data1, Integer[] data2) {
+                return data1[1].compareTo(data2[1]);    // sort on column 2
+                // return data1[0].comapreTo(data2[0]); // sort on column 1
             }
         });
 
-        for (Integer[] i : myarr) {
-            System.out.println(i[0] + "," + i[1]);
+        for (Integer[] elem : myarr) {
+            System.out.println(elem[0] + "," + elem[1]);
+            System.out.println();
         }
-    }
+    } // main() ends
 }
