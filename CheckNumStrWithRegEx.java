446eb9695a4887e9679605613e8963d6a2378025
roottraveller <roottraveller@users.noreply.github.com>
2017-11-01 21:10:06 +0530

    Update CheckNumStrWithRegEx.java

--- CheckNumStrWithRegEx.java
+++ CheckNumStrWithRegEx.java
@@ -12,24 +12,30 @@ import java.util.regex.Pattern;
 
  
 class CheckNumStrWithRegEx{
-	public CheckNumStrWithRegEx() {
+	
+	CheckNumStrWithRegEx() {
 	}
 	
-	private static boolean isNumeric(String str) {
-		String regex = "[0-9]+";  //only digits, one or more OR can use "//d+"
-		Pattern pattern = Pattern.compile(regex);  //pattern to be search; Notice - No new keyword
-		Matcher matcher = pattern.matcher(str);  //matcher - target string 
-		if(matcher.matches() == true) return true;
-		else return false;
+	private static boolean isNumericString(String str) {
+		String regex = "[0-9]+";  // only digits, one or more ||  can use "//d+"
+		Pattern pattern = Pattern.compile(regex);  // pattern to be search; Notice - No new keyword
+		Matcher matcher = pattern.matcher(str);  // matcher - target string 
+		
+		if(matcher.matches() == true) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		
-		String str = new String();
+		String str = ""; // null
 		do {
 			System.out.print("Input : ");		str = sc.nextLine();
-			String status = isNumeric(str) == true ? "Numeric Only" : "Not a Numeric String";
+			
+			String status = isNumericString(str) ? "Numeric String" : "Not a Numeric String";
 			System.out.println(status);
 		} while(!str.equalsIgnoreCase("EXIT"));
 	}
